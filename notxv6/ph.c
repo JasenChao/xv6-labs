f10ea9b9c664b11761144f147b68919a53b68cfc
JasenChao <JasenChao@gmail.com>
2023-11-08 17:00:42 +0800

    thread

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -16,7 +16,7 @@ struct entry {
 struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
-
+pthread_mutex_t lock;
 
 double
 now()
@@ -47,6 +47,7 @@ void put(int key, int value)
     if (e->key == key)
       break;
   }
+  pthread_mutex_lock(&lock);
   if(e){
     // update the existing key.
     e->value = value;
@@ -54,7 +55,7 @@ void put(int key, int value)
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
-
+  pthread_mutex_unlock(&lock);
 }
 
 static struct entry*
@@ -105,7 +106,7 @@ main(int argc, char *argv[])
   void *value;
   double t1, t0;
 
-
+  pthread_mutex_init(&lock, NULL);
   if (argc < 2) {
     fprintf(stderr, "Usage: %s nthreads\n", argv[0]);
     exit(-1);
